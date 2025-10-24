e612e9d0b7bb69d21e7bb3eda5fa0e4d10f777a9
Sang Nguyen <sangnguyenplus@gmail.com>
2025-10-24 11:50:05 +0700

    first commit

---
+++ src/Plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace FriendsOfBotble\SeoMetaKeywords;
+
+use Botble\PluginManagement\Abstracts\PluginOperationAbstract;
+
+class Plugin extends PluginOperationAbstract
+{
+    public static function remove(): void
+    {
+    }
+}
